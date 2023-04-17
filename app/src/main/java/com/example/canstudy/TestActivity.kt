483f2d4c5aeea8e92304a0dde33d2a8c577753ac
stporg <127300104+stporg@users.noreply.github.com>
2023-04-17 17:01:11 +1000

    Finalise functionality to test activity

--- app/src/main/java/com/example/canstudy/TestActivity.kt
+++ app/src/main/java/com/example/canstudy/TestActivity.kt
@@ -1,17 +1,20 @@
 package com.example.canstudy
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.view.View.INVISIBLE
 import android.view.View.VISIBLE
+import android.view.WindowManager
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import com.example.canstudy.databinding.ActivityTestBinding
+import com.example.canstudy.databinding.DialogRestartReviewBinding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
@@ -26,6 +29,7 @@ class TestActivity : AppCompatActivity() {
     private lateinit var ibCross: ImageButton
     private lateinit var btnCheck: Button
     private lateinit var btnReview: Button
+    private lateinit var btnRestart: Button
     private lateinit var tvCantoneseDescription: TextView
     private lateinit var tvCantoneseTranslation: TextView
     private lateinit var tvEnglishDescription: TextView
@@ -34,7 +38,7 @@ class TestActivity : AppCompatActivity() {
     private lateinit var tvRightScore: TextView
     private var correctScore = 0
     private var totalScore = 0
-    private lateinit var wordsChosen: ArrayList<Int>
+    private var wordsChosen = ArrayList<Int>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,6 +52,7 @@ class TestActivity : AppCompatActivity() {
         ibTick.setOnClickListener { calculateCorrectAnswer() }
         ibCross.setOnClickListener { calculateWrongAnswer() }
         btnCheck.setOnClickListener { displayCantoneseText() }
+        btnRestart.setOnClickListener { restartScore() }
     }
 
     private fun calculateCorrectAnswer() {
@@ -55,16 +60,27 @@ class TestActivity : AppCompatActivity() {
         totalScore++
         tvLeftScore.text = correctScore.toString()
         tvRightScore.text = totalScore.toString()
+        tvCantoneseDescription.visibility = INVISIBLE
+        tvCantoneseTranslation.visibility = INVISIBLE
+        getWord()
     }
 
     private fun calculateWrongAnswer() {
         totalScore++
         tvRightScore.text = totalScore.toString()
+        tvCantoneseDescription.visibility = INVISIBLE
+        tvCantoneseTranslation.visibility = INVISIBLE
+        getWord()
     }
 
     private fun displayCantoneseText() {
-        tvCantoneseDescription.visibility = VISIBLE
-        tvCantoneseTranslation.visibility = VISIBLE
+        if (tvCantoneseDescription.visibility == INVISIBLE) {
+            tvCantoneseDescription.visibility = VISIBLE
+            tvCantoneseTranslation.visibility = VISIBLE
+        } else {
+            tvCantoneseDescription.visibility = INVISIBLE
+            tvCantoneseTranslation.visibility = INVISIBLE
+        }
     }
 
     private fun initialiseActivity() {
@@ -72,6 +88,7 @@ class TestActivity : AppCompatActivity() {
         ibCross = binding.ibCross
         btnCheck = binding.btnCheck
         btnReview = binding.btnReview
+        btnRestart = binding.btnRestart
         tvCantoneseDescription = binding.tvCantoneseDescription
         tvCantoneseTranslation = binding.tvCantoneseTranslation
         tvEnglishDescription = binding.tvEnglishDescription
@@ -82,30 +99,78 @@ class TestActivity : AppCompatActivity() {
         tvCantoneseTranslation.visibility = INVISIBLE
         tvCantoneseDescription.visibility = INVISIBLE
 
-        val dao = (application as CanStudyDatabaseApp).db.wordDao()
-
-        getWordList(dao) { wordList ->
-            wordsChosen = ArrayList(wordList.size)
-            val randomValue = Random.nextInt(0, wordList.size - 1)
-            tvEnglishTranslation.text = wordList[randomValue].getEnglishWord()
-            tvCantoneseTranslation.text = wordList[randomValue].getCantoneseWord()
-            wordsChosen.add(randomValue)
-            //Log.e("sushi", "$wordsChosen")
-        }
+        getWord()
     }
 
     private fun getWord() {
         val dao = (application as CanStudyDatabaseApp).db.wordDao()
         getWordList(dao) { wordList ->
-            wordsChosen = ArrayList(wordList.size)
+            // Handle the case where all words have been chosen
+            if (wordsChosen.size == wordList.size) {
+                addRestartReviewDialog()
+                disableElements()
+                tvCantoneseDescription.visibility = VISIBLE
+                tvCantoneseTranslation.visibility = VISIBLE
+                return@getWordList
+            }
+
+            var randomValue: Int
             do {
-                val randomValue = Random.nextInt(0, wordList.size - 1)
-                tvEnglishTranslation.text = wordList[randomValue].getEnglishWord()
-                tvCantoneseTranslation.text = wordList[randomValue].getCantoneseWord()
-                wordsChosen.add(randomValue)
-            } while(!wordsChosen.contains(randomValue))
-            //Log.e("sushi", "$wordsChosen")
+                randomValue = Random.nextInt(0, wordList.size)
+            } while (wordsChosen.contains(randomValue))
+
+            tvEnglishTranslation.text = wordList[randomValue].getEnglishWord()
+            tvCantoneseTranslation.text = wordList[randomValue].getCantoneseWord()
+            wordsChosen.add(randomValue)
         }
+    }
+
+    private fun disableElements() {
+        ibTick.isEnabled = false
+        ibCross.isEnabled = false
+        btnCheck.isEnabled = false
+    }
+
+    private fun enableElements() {
+        ibTick.isEnabled = true
+        ibCross.isEnabled = true
+        btnCheck.isEnabled = true
+    }
+
+    private fun restartScore() {
+        correctScore = 0
+        totalScore = 0
+        tvLeftScore.text = correctScore.toString()
+        tvRightScore.text = totalScore.toString()
+        wordsChosen.clear()
+        enableElements()
+        getWord()
+        tvCantoneseDescription.visibility = INVISIBLE
+        tvCantoneseTranslation.visibility = INVISIBLE
+    }
+
+    private fun addRestartReviewDialog() {
+        val restartReviewDialog = Dialog(this)
+        val dialogBinding = DialogRestartReviewBinding.inflate(layoutInflater)
+        restartReviewDialog.setContentView(dialogBinding.root)
+        restartReviewDialog.setCanceledOnTouchOutside(true)
+
+        // Make dialog match the width of the screen
+        val window = restartReviewDialog.window
+        val layoutParams = window?.attributes
+        layoutParams?.width = WindowManager.LayoutParams.MATCH_PARENT
+        layoutParams?.height = WindowManager.LayoutParams.WRAP_CONTENT
+        window?.attributes = layoutParams
+
+        dialogBinding.btnRestart.setOnClickListener {
+            restartReviewDialog.dismiss()
+            restartScore()
+        }
+        dialogBinding.btnReview.setOnClickListener {
+
+        }
+
+        restartReviewDialog.show()
     }
 
     private fun getWordList(wordDao: WordDao, callback: (ArrayList<WordModel>) -> Unit) {
@@ -127,7 +192,5 @@ class TestActivity : AppCompatActivity() {
             }
         }
     }
-
-
 
 }
